6553498e3e04a24e28d5eaae5d6f915f388a0424
LisaKr <lisa.kramarenko@gmail.com>
2018-06-21 16:53:05 +0200

    Adding first draft

---
+++ main.js
@@ -0,0 +1,65 @@
+$(function(){
+  $("#categories").click(function(event){
+    event.stopPropagation();
+    $("#thirdDropdown").show();
+    $('#dropdown').hide();
+    $('#secondDropdown').hide();
+  });
+});
+
+$(function(){
+  $("#cont").click(function(event){
+    event.stopPropagation();
+    $("#dropdown").show();
+    $('#thirdDropdown').hide();
+    $('#secondDropdown').hide();
+  });
+});
+
+$(function(){
+  $("#about").click(function(event){
+    event.stopPropagation();
+    $("#secondDropdown").show();
+    $('#thirdDropdown').hide();
+    $('#dropdown').hide();
+  });
+});
+
+$(document).click(function(){
+    $('#thirdDropdown').hide();
+    $('#dropdown').hide();
+    $('#secondDropdown').hide();
+});
+
+
+$(function(){
+  $(".button").hover(function(){
+    $(this).toggleClass("highlight");
+  });
+});
+
+$(function(){
+  $(".selection").click(function(){
+  x = $(this).data("category");
+  $(".item").hide();
+  $("."+x).show();
+    });
+});
+
+$(function(){
+  $("ul:first-child").click(function(){
+    $(".item").show();
+    $(".hidden").hide();
+  });
+});
+
+
+$(document).ready ( function () {
+    $(document).on ("click", "img", function () {
+        alert("hi");
+    });
+});
+
+/* $(function() {
+		$(".fancybox").fancybox();
+	}); */
